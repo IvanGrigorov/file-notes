ff2bb27350f1478f81a45052745b1d9b72891e1d
Ivan Grigorov <ivangrigorov9@gmail.com>
2022-12-15 00:10:18 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,10 @@
+# Features
+
+
+With this extension you can save notes per file. This notes can be specifically created for the file. Changes history or more comprihensive TODOs.
+
+## `Important` if you change the location of your project you will need to recreate your notes.
+
+
+
+<a href="https://www.flaticon.com/free-icons/essay" title="essay icons">Essay icons created by Freepik - Flaticon</a>
---
+++ src/extension.ts
@@ -0,0 +1,59 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let opennotes = vscode.commands.registerCommand('file-notes.openNotes', () => {
+		openNotes(context);
+	});
+
+	let savenotes = vscode.commands.registerCommand('file-notes.saveNotes', () => {
+		saveNotes(context);
+	});
+
+
+	context.subscriptions.push(opennotes);
+	context.subscriptions.push(savenotes);
+}
+
+function saveNotes(context: vscode.ExtensionContext) {
+	let currentTabTitle =  vscode.window.tabGroups.activeTabGroup.activeTab?.input as any;
+	currentTabTitle = currentTabTitle.uri.path	|| '';
+	if (!currentTabTitle.includes('Notes-')) {
+		vscode.window.showErrorMessage("Cannot save notes. Not a notes file.");
+		return;
+	}
+	else {
+		const key = currentTabTitle.split('Notes-').pop();
+		context.globalState.update(key, vscode.window.activeTextEditor?.document.getText());
+		vscode.window.showInformationMessage("Notes saved yopu can now close the file without saving it.");
+	}
+}
+
+function openNotes(context: vscode.ExtensionContext) {
+	let file = vscode.window.activeTextEditor!.document.fileName || '';
+	if (!file) {
+		vscode.window.showErrorMessage("You can open notes only for saved files");
+		return;
+	}
+	var setting: vscode.Uri = vscode.Uri.parse('untitled:Notes-' + file);
+	vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
+		vscode.window.showTextDocument(a, 1, false).then(e => {
+			e.edit(edit => {
+				edit.insert(new vscode.Position(0, 0),  context.globalState.get(file) || '');
+			});
+		});
+	}, (error: any) => {
+		console.error(error);
+		debugger;
+	});
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+
+//"onCommand:file-notes.openNotes",
+//"onCommand:file-notes.saveNotes"
